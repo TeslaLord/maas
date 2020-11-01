2faeff14d0b44b8e9f2a91abf72df809b80e26a7
TeslaLord <ntharun@gmail.com>
2020-11-01 19:50:23 +0530

    Added desc

--- modelservice/settings.py
+++ modelservice/settings.py
@@ -129,9 +129,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# MEDIA_URL = '/images/'
+MEDIA_URL = '/images/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/speech")
