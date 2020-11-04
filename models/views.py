429a8d4a8000b705a910fe4e62c05a04f8681990
TeslaLord <ntharun@gmail.com>
2020-11-04 13:20:16 +0000

    Revert "test"
    This reverts commit d96a797b989bb02c6c159399b0dbd1049c6de608.

--- models/views.py
+++ models/views.py
@@ -101,7 +101,6 @@ def speech_to_text(request):
 
 def pdf(request):
     if request.method == 'POST':
-        print('posting.......')
         """FUNCTION TO SHOW THE IMAGE"""
         def show_img(img_list):
             for j, img in enumerate(img_list):
